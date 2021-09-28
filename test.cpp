5a5f03f36021db6fa77044c32b1093eafe494190
shuk-ori <ggochi92@naver.com>
2021-09-29 00:18:08 +0900

    first commit

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+
+    int a = 10;
+    int b = 100;
+    std::cout << a+b << '\n';   
+   
+    std::cout << "Hello, World!!!" << std::endl;
+   
+    return 0;
+
+}
